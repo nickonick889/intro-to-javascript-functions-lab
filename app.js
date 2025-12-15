1ebec425663a7176f12ec103ef392cc660aa5680
Nicholas Lai <nicholaslai889@gmail.com>
2025-12-15 23:24:20 +0800

    First commit First commit

---
+++ app.js
@@ -0,0 +1,33 @@
+// Exercise 1  
+const maxOfTwoNumbers = (x, y) => {
+  if (x >= y) {
+    return x;
+  } else {
+    return y;
+  }
+}
+
+console.log('Exercise 1 Result:', maxOfTwoNumbers(3, 9));
+
+//Excercise 2 
+const isAdult = (z) => {
+    if (z => 18) {
+    return 'Adult';
+    } else {
+        return 'minor';
+    }
+}
+
+console.log('Exercise 2 Result:', isAdult(21));
+
+// Exercise 3
+
+
+
+// Exercise 4 
+
+// Exercise 5 
+
+// Excercsie 6 
+
+// Excersie 7
